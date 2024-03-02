b6981f179f7aff87029859e2b0bb1ff43df31426
Jonghyeok K <jka236@sfu.ca>
2024-03-02 15:59:47 -0800

    resource update

--- kpo_example.py
+++ kpo_example.py
@@ -3,6 +3,23 @@ from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
     KubernetesPodOperator,
 )
 from airflow.utils.dates import days_ago
+from kubernetes.client import models as k8s
+
+resources_scala = k8s.V1ResourceRequirements(
+    requests={"memory": "64Mi", "cpu": "250m"},
+    limits={
+        "memory": "64Mi",
+        "cpu": 0.25,
+    },
+)
+
+resources_python = k8s.V1ResourceRequirements(
+    requests={"memory": "64Mi", "cpu": "250m", "ephemeral-storage": "1Gi"},
+    limits={
+        "memory": "128Mi",
+        "cpu": 1,
+    },
+)
 
 default_args = {
     "owner": "airflow",
@@ -23,6 +40,7 @@ with DAG(
         image="spark:scala",
         cmds=["bash", "-cx"],
         arguments=["/opt/spark/bin/spark-shell --version"],
+        resources=resources_scala,
     )
 
     spark_python = KubernetesPodOperator(
@@ -32,6 +50,7 @@ with DAG(
         image="spark:python3",
         cmds=["bash", "-cx"],
         arguments=["/opt/spark/bin/pyspark", "--version"],
+        resources=resources_python,
     )
 
     spark_python >> spark_scala
